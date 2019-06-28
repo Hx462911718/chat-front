1ea120fcb5aa39366faafdb1451eee8615842db0
hexin <hx042412@163.com>
2019-06-28 11:30:03 +0800

    样式

--- src/pages/chat/home/components/side/side.js
+++ src/pages/chat/home/components/side/side.js
@@ -15,6 +15,7 @@ class Sidebar extends Component{
     const {socket} = this.props;
     this.state = {
       filterKey:"",
+      zIndex:1,
 
     };
     socket.on('connect', function () {
@@ -28,6 +29,12 @@ class Sidebar extends Component{
     let {value}=e.target;
       this.setState({
         filterKey:value
+      })
+  }
+  changeComponents=(index)=>{
+
+      this.setState({
+        zIndex:index
       })
   }
   render(){
@@ -51,7 +58,11 @@ class Sidebar extends Component{
             <a className="add" onClick={()=>{searchUser()}}><Icon type="plus-circle" /></a>
           </footer>
         </div>
-        <List {...listProps}/>
+        {this.state.zIndex === 1 ? <List {...listProps}/>:<div style={{height:475}}></div>}
+        <div style={{height:"30px",borderTop:"2px solid #2a89cf"}}>
+          <a onClick={()=>{this.changeComponents(1)}} style={{color:"#86ff49",fontSize:25}} title={"我的好友"}><Icon type="user" /></a>
+          <a onClick={()=>{this.changeComponents(2)}} style={{color:"#86ff49",fontSize:25,marginLeft:20}} title={"新朋友"}> <Icon type="message" /></a>
+        </div>
       </section>
     );
   }
